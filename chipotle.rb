7a9403e8f4bfed7f7d2bcc90e7a3cb1e265d3e91
Alden Sulger <asulger@lsoc.org>
2016-02-10 16:04:31 -0600

    got up to step six

--- chipotle.rb
+++ chipotle.rb
@@ -1,5 +1,5 @@
 #The menu board at Chipotle Reads: 
-main = {
+menu = {
 	style: ["burrito", "bowl", "crispy corn tacos", "soft corn tacos", "soft flour tacos", "salad"],
 	meat: ["steak", "carnitas", "chicken", "barbacoa", "sofritas"],
 	rice_bean_veg: ["cilantro-lime brown rice", "cilantro-lime white rice", "black beans", "pinto beans", "fajita veggies"],
@@ -22,27 +22,36 @@ my_order = {}
 #STEP ONE:
 	#you are in line at chipotle and what to know what different styles they have
 	#Display all the options available for style 
-
+ puts menu[:style] 
+ 
 
 
 #STEP TWO:
 	#Using the empty my_order hash, 
 	#Chose your style from the available options and add a new key value pair to that hash
-
-
+my_order[:style] = menu[:style][1]
+puts "I will start with a " + my_order[:style]
 #STEP THREE:
 	#Chose what meat you want & add it to your my_order hash
-
+puts "What meat do you want in your" + my_order[:stlye].to_s + "?"
+my_order[:meat] = menu[:meat][2]
+puts "I would love a "  + my_order[:meat] + " " + my_order[:style] + " please!"
 #STEP FOUR:
 	#Chose what rice, bean or veggies you want and it it as a new key value pair to my_order. 
 		#HINT: You may want to chose more than one option, how will you store multiples in your order?
-
+puts "What are the available beans, veggies, and rice?"
+puts menu [:rice_bean_veg]
+my_order[:rice_bean_veg] = menu[:rice_bean_veg][1] + " and " + menu[:rice_bean_veg][3]
+puts "Can you add " + my_order[:rice_bean_veg] + " to my " + my_order[:style] + " please!"
 #STEP FIVE
 	#Check to see if your favorite topping is included in the available options
-
+puts menu [:toppings].include?("guacamole")
 
 #STEP SIX
 	#Chose at least 3 toppings to add to your order
+	my_order[:toppings] = []
+	
+	
 
 #STEP SEVEN
 	#Oops! You didn't want one of those toppings! How do you remove the second one you put in there?
@@ -51,7 +60,7 @@ my_order = {}
 #STEP EIGHT
 	#Print your hash and see if your order was made correctly! Bon Appetit! 
 
-
+puts my_order
 
 
 
@@ -61,7 +70,7 @@ my_order = {}
 
 	#Example: "Can I please have a burrito. For meat, I would like chicken with cilantro-lime white rice and black beans. Then I would like sour cream, cheese and roasted-corn salsa as my toppings. Thank you!"
 
-
+puts "Hello sir. I would love a "  + my_order[:meat] + " " + my_order[:style] + " with " + my_order[:rice_bean_veg] + " and can you please add " 
 
 #### BONUS 2 #####
 	#Make this into a command line application that allows the user to input their order 
